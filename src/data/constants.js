dbf19b39213574d59a3cf0ad08f7bba9fb6c2c50
RADIN MNX <153691667+RADINMNX2@users.noreply.github.com>
2026-09-12 05:46:17 +0330

    chore(content): sync store settings from Rozhina Admin Studio

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,14 +1,13 @@
-export const WHATSAPP_NUMBER = '989123456789';
+export const WHATSAPP_NUMBER = "989123456789";
 
-export const INSTAGRAM_HANDLE = '@rozhina.scarf';
+export const INSTAGRAM_HANDLE = "@rozhina.scarf";
 
-export const INSTAGRAM_URL = 'https://instagram.com/rozhina.scarf';
+export const INSTAGRAM_URL = "https://instagram.com/rozhina.scarf";
 
 export const FREE_SHIPPING_THRESHOLD = 1000000;
 
-export const ANNOUNCEMENT_TEXT =
-  'ارسال رایگان به سراسر کشور برای خریدهای بالای ۱ میلیون تومان';
+export const ANNOUNCEMENT_TEXT = "ارسال رایگان به سراسر کشور برای خریدهای بالای ۱ میلیون تومان";
 
-export const PHONE_NUMBER = '۰۹۱۲ ۳۴۵ ۶۷۸۹';
+export const PHONE_NUMBER = "۰۹۱۲ ۳۴۵ ۶۷۸۹";
 
-export const SUPPORT_ID = '@rozhina.support';
+export const SUPPORT_ID = "@rozhina.support";
